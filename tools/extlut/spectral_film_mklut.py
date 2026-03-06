6b3fc425fd6f727d7dd6bc8b9e8222b0f5f79072
Alberto Griggio <alberto.griggio@gmail.com>
2026-03-06 13:54:43 +0100

    updated "spectral film" external LUT to the latest version of the lib

--- tools/extlut/spectral_film_mklut.py
+++ tools/extlut/spectral_film_mklut.py
@@ -6,9 +6,113 @@ import json
 import traceback
 from contextlib import redirect_stdout, redirect_stderr
 
-FIXED_STOCKS = True
+try:
+    from spectral_film_lut.film_data import FilmData
+    HAS_FILM_DATA = True
+except ImportError:
+    HAS_FILM_DATA = False
 
-if FIXED_STOCKS:
+
+if HAS_FILM_DATA:
+    films = [
+        "Agfa Vista 100",
+        "Fuji C200",
+        "Fuji Eterna 500",
+        "Fuji Eterna 500 Vivid",
+        "Fuji FP-100C",
+        "Fuji Instax color",
+        "Fuji Natura 1600",
+        "Fuji Pro 160C",
+        "Fuji Pro 160S",
+        "Fuji Pro 400H",
+        "Fuji Provia 100F",
+        "Fuji Superia Reala",
+        "Fuji Superia X-Tra 400",
+        "Fuji Velvia 50",
+        "Kodachrome 64",
+        "Kodak Vision3 250D 5207",
+        "Kodak Vision3 500T 5219",
+        "Kodak 5222 Dev 12",
+        "Kodak 5222 Dev 4",
+        "Kodak 5222 Dev 5",
+        "Kodak 5222",
+        "Kodak 5222 Dev 9",
+        "Kodak 5247",
+        "Kodak 5247 II",
+        "Kodak 5248",
+        "Kodak 5250",
+        "Kodak Vision 320T 5277",
+        "Kodak EXR 200T 5293",
+        "Kodak Aerochrome III Infrared Film 1443",
+        "Kodak Aerocolor IV 2460",
+        "Kodak Aerocolor IV 2460 High",
+        "Kodak Aerocolor IV 2460 Low",
+        "KodakDyeTransferSeparation",
+        "Kodak EXR 100T 5248",
+        "Kodak Ektachrome 100D",
+        "Kodak Ektar 100",
+        "Kodak Gold 200",
+        "Kodak Portra 160",
+        "Kodak Portra 400",
+        "Kodak Portra 800",
+        "Kodak Portra 800 @1600",
+        "Kodak Portra 800 @3200",
+        "Kodak Trix-X 400 Dev 11",
+        "Kodak Tri-X 400",
+        "Kodak Trix-X 400 Dev 7",
+        "Kodak Trix-X 400 Dev 9",
+        "Kodak Ultramax 500",
+        "Kodak Vericolor III",
+        "Technicolor IV",
+        "Technicolor IV Alt. 1",
+        "Technicolor IV Alt. 2",
+        "Kodak Vision3 50D 5203",
+        "Kodak Vision3 200T 5213",
+        "Kodak 5247 II Alt",
+    ]
+    papers = [
+        "Fuji Eterna-CP Type 3513DI",
+        "Fuji Crystal Archive DPII",
+        "Fuji Crystal Archive Maxima",
+        "Fuji Crystal Archive Super Type C",
+        "Fujiflex Crystal Archive New Version",
+        "Fujiflex Crystal Archive Old Version",
+        "Kodak 2302 Dev 2",
+        "Kodak 2302 Dev 3",
+        "Kodak 2302 Dev 5",
+        "Kodak 2302 Dev 7",
+        "Kodak 2302 Dev 9",
+        "Kodak Vision 2383",
+        "Kodak Vision Premier 2393",
+        "Kodak 5381",
+        "Kodak 5383",
+        "Kodak 5384",
+        "Kodak Duraflex Plus",
+        "KodakDyeTransferKodachrome",
+        "KodakDyeTransferNegative",
+        "Kodak Endura Premier Paper",
+        "Kodak EXR 5386",
+        "Kodak Professional Polymax Fine-Art Paper",
+        "Kodak Polymax Fine-Art Paper Grade 0",
+        "Kodak Polymax Fine-Art Paper Grade 1",
+        "Kodak Polymax Fine-Art Paper Grade 2",
+        "Kodak Polymax Fine-Art Paper Grade 3",
+        "Kodak Polymax Fine-Art Paper Grade 4",
+        "Kodak Polymax Fine-Art Paper Grade 5",
+        "Kodak Polymax Fine-Art Paper Grade -1",
+        "Kodak Portra Endura Paper",
+        "Kodka Supra Endura Paper",
+        "Technicolor V",
+        "None",
+        "Fuji Crystal Archive Pro PDII",
+        "Ilfochrome Micrographic M",
+        "Ilfochrome Micrographic P",
+        "KodakDyeTransferSlide",
+        "Kodak Ektachrome Radiance III Paper",
+        "Fuji Eterna-CP 3523XD",
+        "Kodak 2302",
+    ]
+else:
     films = [
         "AgfaVista100",
         "FujiC200",
@@ -108,24 +212,31 @@ if FIXED_STOCKS:
 with warnings.catch_warnings(action='ignore'):
     from spectral_film_lut.utils import create_lut
     try:
-        from spectral_film_lut import FILMSTOCKS
+        from spectral_film_lut.utils import to_numpy
     except ImportError:
-        import spectral_film_lut.film_loader
-        FILMSTOCKS = {cls.__name__ : cls
-                      for cls in spectral_film_lut.film_loader.filmstocks}
-    if not FIXED_STOCKS:
-        films = sorted(f for f in FILMSTOCKS
-                       if FILMSTOCKS[f]().stage == 'camera')
-        papers = sorted(f for f in FILMSTOCKS
-                        if FILMSTOCKS[f]().stage == 'print') + ['None']
+        def to_numpy(x): return x
+        
+    if HAS_FILM_DATA:
+        import spectral_film_lut
+        from spectral_film_lut.film_spectral import FilmSpectral
+        def mkstock(s): return lambda : FilmSpectral(s)
+        FILMSTOCKS = { s.name : mkstock(s)
+                       for s in  spectral_film_lut.FILM_STOCKS }
+    else:
+        try:
+            from spectral_film_lut import FILMSTOCKS
+        except ImportError:
+            import spectral_film_lut.film_loader
+            FILMSTOCKS = {cls.__name__ : cls
+                          for cls in spectral_film_lut.film_loader.filmstocks}
     FILMSTOCKS['None'] = lambda : None
 
 
 def getopts():
     p = argparse.ArgumentParser()
     p.add_argument('--server', action='store_true')
-    p.add_argument('--film', choices=films, default=films[0])
-    p.add_argument('--paper', choices=papers, default=papers[0])
+    p.add_argument('--film', choices=films)
+    p.add_argument('--paper', choices=papers)
     p.add_argument('--exposure', type=float, default=0)
     p.add_argument('--wb', type=int, default=6500)
     p.add_argument('--tint', type=float, default=0)
@@ -137,9 +248,56 @@ def getopts():
     p.add_argument('--sat', type=float, default=1)
     p.add_argument('--black-offset', type=float, default=0)
     p.add_argument('--print-stocks', action='store_true')
+    p.add_argument('--print-stocks-verbose', action='store_true')
     p.add_argument('params', nargs='?')
     p.add_argument('output', nargs='?')
-    return p.parse_args()
+    res = p.parse_args()
+    if res.print_stocks_verbose:
+        res.print_stocks = True
+    return res
+
+
+def print_stocks(verbose):
+    out_films = []
+    out_papers = []
+    seen = set()
+    for i, f in enumerate(films):
+        seen.add(f)
+        if f in FILMSTOCKS:
+            out_films.append((f, i))
+        else:
+            out_films.append((f, -1))
+    for i, p in enumerate(papers):
+        seen.add(p)
+        if p in FILMSTOCKS:
+            out_papers.append((p, i))
+        else:
+            out_papers.append((p, -1))
+    extra_films = []
+    extra_papers = []
+    for k in sorted(FILMSTOCKS):
+        if k not in seen:
+            if FILMSTOCKS[k]().stage == 'camera':
+                extra_films.append((k, len(films) + len(extra_films)))
+            else:
+                extra_papers.append((k, len(papers) + len(extra_papers)))
+    def sortkey(t):
+        return t[1] < 0, t[0]
+    out_films.sort(key=sortkey)
+    out_papers.sort(key=sortkey)
+    if verbose:
+        for l in (out_films, out_papers, extra_films, extra_papers):
+            for i, k in enumerate(l):
+                if k[1] >= 0:
+                    s = FILMSTOCKS[k[0]]()
+                    if s is not None:
+                        l[i] = (f'{k[0]} ({s.film_type.capitalize()})', k[1])
+    def prl(l):
+        return ',\n  '.join(f'["{k[0]}", {k[1]}]' for k in l)
+    print(f'films = [\n  {prl(out_films)}\n]\n')
+    print(f'papers = [\n  {prl(out_papers)}\n]\n')
+    print(f'extra_films = [\n  {prl(extra_films)}\n]\n')
+    print(f'extra_papers = [\n  {prl(extra_papers)}\n]\n')
 
 
 def update_params(params, fname):
@@ -173,8 +331,9 @@ def get_params(opts):
 
 
 def mklut(params, output):
-    lut = create_lut(FILMSTOCKS[params['film']](),
-                     FILMSTOCKS[params['paper']](),
+    def default(): return None
+    lut = create_lut(FILMSTOCKS.get(params['film'], default)(),
+                     FILMSTOCKS.get(params['paper'], default)(),
                      lut_size=33,
                      cube=False,
                      input_colourspace='ACEScct',
@@ -193,6 +352,7 @@ def mklut(params, output):
                      sat_adjust=params['sat'],
                      matrix_method=False,
                      )
+    lut = to_numpy(lut)[...,:3]
     dx, dy, dz = lut.shape[:-1]
     table = lut.reshape((-1, 3))
     with open(output, 'w') as out:
@@ -225,9 +385,7 @@ def main():
     opts = getopts()
 
     if opts.print_stocks:
-        print(f'films = {json.dumps(films, indent=2)}')
-        print(f'papers = {json.dumps(papers, indent=2)}')
-        exit(0)
+        return print_stocks(opts.print_stocks_verbose)
     
     params = get_params(opts)
     if opts.server:
