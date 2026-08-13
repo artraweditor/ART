b93a76e51811f3474642afba05b2017e33c68058
Alberto Griggio <alberto.griggio@gmail.com>
2026-08-13 08:27:44 +0200

    dcraw: better quicktime atom boundary validation (issue #513)

--- rtengine/dcraw.cc
+++ rtengine/dcraw.cc
@@ -7775,21 +7775,27 @@ void CLASS parse_riff()
 
 void CLASS parse_qt (int end)
 {
-  unsigned save, size;
+  long save;
+  unsigned size, next;
   char tag[4];
 
   order = 0x4d4d;
   while (ftell(ifp)+7 < end) {
     save = ftell(ifp);
     if ((size = get4()) < 8) return;
+    if (save < 0 || save > end)
+        return;
+    if (size > (unsigned)(end - save))
+        return;
+    next = unsigned(save) + size;
     fread (tag, 4, 1, ifp);
     if (!memcmp(tag,"moov",4) ||
 	!memcmp(tag,"udta",4) ||
 	!memcmp(tag,"CNTH",4))
-      parse_qt (save+size);
+      parse_qt (next);
     if (!memcmp(tag,"CNDA",4))
       parse_jpeg (ftell(ifp));
-    fseek (ifp, save+size, SEEK_SET);
+    fseek (ifp, next, SEEK_SET);
   }
 }
 
